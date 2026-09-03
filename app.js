ee7d0879ec6b777f3b992c0c2fa376c8f0f80116
ivazquezromero2090 <ivazquezromero2090@gmail.com>
2026-09-03 13:04:47 +0200

    Añadidas credenciales reales de Supabase

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // ==========================================
 // ☁️ CONFIGURACIÓN Y CONEXIÓN CON SUPABASE
 // ==========================================
-const SUPABASE_URL = "https://TU_PROJECT_URL_AQUÍ.supabase.co"; 
-const SUPABASE_ANON_KEY = "TU_API_KEY_PUBLICA_AQUÍ";
+const SUPABASE_URL = "https://hlrlyyvddvtbvmzariuf.supabase.co"; 
+const SUPABASE_ANON_KEY = "sb_publishable_X-hpA6s1Zeo7aLpT608fIQ_ig_qB0Ie";
 
 // Creamos el intérprete oficial que hablará con tu base de datos
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
